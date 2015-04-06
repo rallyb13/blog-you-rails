4d2073d26d3e908cf7b5e497362456572440606b
Benjamin Herson <benjaminherson@gmail.com>
2015-04-05 21:13:21 -0700

    fix how phone number is referenced in comment model, adjust readme

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,20 +2,21 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   validates :content, :presence => true
   belongs_to :user
-  before_create :send_sms
+  # before_create :send_sms
 
   private
 
     def send_sms
+# binding.pry
       response = RestClient::Request.new(
-      :method => post,
-      :url => 'https://api.twilio.com/2010-04-01/Accounts/#{ENV["TWILIO_ACCOUNT_SID"]}/Messages.json',
+      :method => :post,
+      :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
       :user => ENV['TWILIO_ACCOUNT_SID'],
       :password => ENV['TWILIO_AUTH_TOKEN'],
       :payload => {
         Body: "You have a new comment",
-        To: @post.user.phone,
-        From: 9713202132 }
+        To: self.post.user.phone,
+        From: "9713202132" }
       ).execute
     end
 
